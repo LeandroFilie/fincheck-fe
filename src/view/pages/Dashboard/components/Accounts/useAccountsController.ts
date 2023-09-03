cd91d2b6568e920a0a5dc2e28afeacc739f0d94f
LeandroFilie <leandro.gfilie@gmail.com>
2023-09-03 13:58:45 -0300

    feat: added responsive in slider

--- src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
+++ src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
@@ -1,10 +1,13 @@
+import { useWindowWidth } from '@app/hooks/useWindowWidth';
 import { useState } from 'react';
 
 export function useAccountsController() {
   const [sliderState, setSliderState] = useState({ isBeginning: true, isEnd: false });
+  const windowWidth = useWindowWidth();
 
   return {
     sliderState,
     setSliderState,
+    windowWidth,
   };
 }
