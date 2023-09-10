56666d6973a5a474010a56f63fa782f895ce6e22
LeandroFilie <leandro.gfilie@gmail.com>
2023-09-10 01:21:49 -0300

    feat: create dropdown menu in user menu

--- src/view/components/DropdownMenu.tsx
+++ src/view/components/DropdownMenu.tsx
@@ -28,7 +28,7 @@ function DropdownMenuContent({ children, className }: DropdownMenuProps) {
     <RdxDropdownMenu.Portal>
       <RdxDropdownMenu.Content
         className={cn(
-          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]',
+          'rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slide-up-and-fade',
           className,
         )}
       >
@@ -38,11 +38,16 @@ function DropdownMenuContent({ children, className }: DropdownMenuProps) {
   );
 }
 
-function DropdownMenuItem({ children, className }: DropdownMenuProps) {
+interface DropdownMenuItemProps extends DropdownMenuProps{
+  onSelect: () => void;
+}
+
+function DropdownMenuItem({ children, className, onSelect }: DropdownMenuItemProps) {
   return (
     <RdxDropdownMenu.Item
+      onSelect={onSelect}
       className={cn(
-        'min-h-[48px] outline-none flex items-center py-2 px-4 text-sm text-gray-800 hover:bg-gray-50 rounded-2xl transition-colors cursor-pointer',
+        'min-h-[48px] outline-none flex items-center py-2 px-4 text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl transition-colors cursor-pointer',
         className,
       )}
     >
