e020ed0d51a20cfc0f9af901650ddc5859c71b2b
LeandroFilie <leandro.gfilie@gmail.com>
2023-09-10 19:06:32 -0300

    feat: added radix modal

---
+++ src/view/components/Modal/Content.tsx
@@ -0,0 +1,5 @@
+export function Content() {
+  return (
+    <h1>Conteudo</h1>
+  );
+}
---
+++ src/view/components/Modal/index.tsx
@@ -0,0 +1,9 @@
+import { Root } from './Root';
+import { Header } from './Header';
+import { Content } from './Content';
+
+export const Modal = {
+  Root,
+  Header,
+  Content,
+};
